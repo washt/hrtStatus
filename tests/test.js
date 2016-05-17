940a19809f6f08e462fd1bde9c0154deed02bf87
washt <washt23@gmail.com>
2016-05-17 18:44:14 -0400

    added tests

---
+++ tests/test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var assert = require('assert');
+
+describe('HRTStatus', function() {
+  describe('authentication', function () {
+    it('should successfully authenticate with gmail', function () {
+			//
+    });
+  });
+  describe('feed quality', function () {
+		it('detect a stale feed', function () {
+			//
+		});
+		it('detect an empty feed', function () {
+			//
+		});
+		 it('detect an fresh feed', function () {
+			//
+		});
+ });
+  describe('modules', function () {
+    it('should successfully load dependencies', function () {
+			//
+    });
+  });
+
+});
