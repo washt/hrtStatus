3b5b47b1cf163355981c204241f61f3195769e96
washt <washt23@gmail.com>
2016-05-19 00:18:29 -0400

    stumped tests

--- tests/test.js
+++ tests/test.js
@@ -1,27 +1,31 @@
 'use strict';
 
-var assert = require('assert');
+var assert = require('assert'),
+	  hrtStatus = require('../hrtStatus.js'),
+
 
 describe('HRTStatus', function() {
   describe('authentication', function () {
     it('should successfully authenticate with gmail', function () {
-			//
+			var transporter = nodemailer.createTransport(config['mailauth'])
+
+		  assert.equal(transporter,true);	
     });
   });
   describe('feed quality', function () {
 		it('detect a stale feed', function () {
-			//
+		  assert.equal(1,2);	
 		});
 		it('detect an empty feed', function () {
-			//
+		  assert.equal(1,2);	
 		});
 		 it('detect an fresh feed', function () {
-			//
+		  assert.equal(1,2);	
 		});
  });
   describe('modules', function () {
     it('should successfully load dependencies', function () {
-			//
+		  assert.equal(1,2);	
     });
   });
 
